5fb5c1d2799e01ef7a332150c754242d300d1780
JamesPerlman <jam.e.perl@gmail.com>
2019-08-05 20:17:45 -0500

    Initial Commit

---
+++ FindEveryone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindEveryone
+//
+//  Created by James Perlman on 8/5/19.
+//  Copyright © 2019 Salida Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
